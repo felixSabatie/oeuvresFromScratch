b1cc31364f4bbb961434253dc6e15b742ed22cdf
Irindul <mathieu.regnard2@gmail.com>
2018-03-12 14:33:44 +0100

    CRUD for Adherent

--- src/main/java/fr/polytech/oeuvres/repositories/OeuvrePretRepository.java
+++ src/main/java/fr/polytech/oeuvres/repositories/OeuvrePretRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OeuvrePretRepository extends JpaRepository<OeuvrePret, Long> {
+public interface OeuvrePretRepository extends JpaRepository<OeuvrePret, Integer> {
 
 }
