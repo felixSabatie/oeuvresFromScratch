476c7dd3dd20a3879ebbdd8d03837fbf4f174b84
Irindul <mathieu.regnard2@gmail.com>
2018-03-14 10:26:42 +0100

    Proprietaire controller CRUD

--- src/main/java/fr/polytech/oeuvres/controllers/ProprietaireController.java
+++ src/main/java/fr/polytech/oeuvres/controllers/ProprietaireController.java
@@ -1,10 +1,17 @@
 package fr.polytech.oeuvres.controllers;
 
+import fr.polytech.oeuvres.exceptions.ResourceNotFoundException;
+import fr.polytech.oeuvres.models.Adherent;
 import fr.polytech.oeuvres.models.Proprietaire;
 import fr.polytech.oeuvres.repositories.ProprietaireRepository;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +24,33 @@ public class ProprietaireController extends Controller {
 		this.proprietaireRepository = proprietaireRepository;
 	}
 
-	@RequestMapping("/proprietaires")
+	@GetMapping("/proprietaires")
 	public List<Proprietaire> getAllOeuvrePret() {
 		return proprietaireRepository.findAll();
+	}
+
+	@GetMapping("/proprietaires/{id}")
+	public Proprietaire getProprietaireById(@PathVariable(value = "id") int id) {
+		return proprietaireRepository.findById(id).orElseThrow(
+			() -> new ResourceNotFoundException("Proprietaire", "id", id)
+		);
+	}
+
+	@PostMapping("/proprietaires")
+	public Proprietaire createProprietaire(@Valid @RequestBody Proprietaire proprietaire){
+		return proprietaireRepository.save(proprietaire);
+	}
+
+	@PutMapping("/proprietaires/{id}")
+	public Proprietaire modifyProprietaire(@PathVariable(value = "id") int id, @
+		Valid @RequestBody Proprietaire proprietairePayload) {
+		Proprietaire proprietaire = proprietaireRepository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("Adherent", "id", id));
+
+		proprietaire.setNomProprietaire(proprietairePayload.getNomProprietaire());
+		proprietaire.setPrenomProprietaire(proprietairePayload.getPrenomProprietaire());
+		proprietaire.setOeuvresPret(proprietairePayload.getOeuvresPret());
+
+		return proprietaireRepository.save(proprietaire);
 	}
 }
