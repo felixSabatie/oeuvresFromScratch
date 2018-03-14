58addd0bc0d7847d247111c5a77f6c2c7c152908
Félix Sabatié <felix.sabatie@gmail.com>
2018-03-14 10:29:15 +0100

    OeuvreVente controller

--- src/main/java/fr/polytech/oeuvres/controllers/OeuvreVenteController.java
+++ src/main/java/fr/polytech/oeuvres/controllers/OeuvreVenteController.java
@@ -1,9 +1,16 @@
 package fr.polytech.oeuvres.controllers;
 
+import fr.polytech.oeuvres.exceptions.ResourceNotFoundException;
 import fr.polytech.oeuvres.models.OeuvreVente;
 import fr.polytech.oeuvres.repositories.OeuvreVenteRepository;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +24,35 @@ public class OeuvreVenteController extends Controller {
 		this.oeuvreVenteRepository = oeuvreVenteRepository;
 	}
 
-	@RequestMapping("/oeuvreventes")
-	public List<OeuvreVente> getAllOeuvrePret() {
+	@RequestMapping("/oeuvresvente")
+	public List<OeuvreVente> getAllOeuvresVente() {
 		return oeuvreVenteRepository.findAll();
+	}
+
+	@PostMapping("/oeuvresvente")
+	public OeuvreVente createOeuvreVente(@Valid @RequestBody OeuvreVente oeuvreVente) {
+		return oeuvreVenteRepository.save(oeuvreVente);
+	}
+
+	@GetMapping("/oeuvresvente/{id}")
+	public OeuvreVente getOeuvreVenteById(@PathVariable(value = "id") int oeuvrePretId) {
+		return oeuvreVenteRepository.findById(oeuvrePretId).orElseThrow(
+				() -> new ResourceNotFoundException("OeuvreVente", "id", oeuvrePretId)
+		);
+	}
+
+	@PutMapping("/oeuvresvente/{id}")
+	public OeuvreVente modifyOeuvreVente(@PathVariable(value = "id") int id,
+			@Valid @RequestBody OeuvreVente oeuvreVentePayload) {
+
+		OeuvreVente oeuvreVente = oeuvreVenteRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("OeuvreVente", "id", id));
+
+		oeuvreVente.setEtatOeuvrevente(oeuvreVentePayload.getEtatOeuvrevente());
+		oeuvreVente.setPrixOeuvrevente(oeuvreVentePayload.getPrixOeuvrevente());
+		oeuvreVente.setReservations(oeuvreVentePayload.getReservations());
+		oeuvreVente.setTitreOeuvrevente(oeuvreVentePayload.getTitreOeuvrevente());
+
+		return oeuvreVenteRepository.save(oeuvreVente);
 	}
 }
