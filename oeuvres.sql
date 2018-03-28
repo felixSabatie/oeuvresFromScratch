99577c41beacc74b426c434e8bc45f1da9ea2433
Félix Sabatié <felix.sabatie@gmail.com>
2018-03-28 16:39:28 +0200

    Élagage

--- oeuvres.sql
+++ oeuvres.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  db
--- Généré le :  Mer 14 Mars 2018 à 10:17
+-- Généré le :  Mer 28 Mars 2018 à 14:38
 -- Version du serveur :  5.7.16
 -- Version de PHP :  5.6.28
 
@@ -51,26 +51,6 @@ INSERT INTO `adherent` (`id_adherent`, `nom_adherent`, `prenom_adherent`, `ville
 -- --------------------------------------------------------
 
 --
--- Structure de la table `oeuvrepret`
---
-
-CREATE TABLE `oeuvrepret` (
-  `id_oeuvrepret` int(10) UNSIGNED NOT NULL,
-  `titre_oeuvrepret` varchar(200) NOT NULL,
-  `id_proprietaire` int(10) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `oeuvrepret`
---
-
-INSERT INTO `oeuvrepret` (`id_oeuvrepret`, `titre_oeuvrepret`, `id_proprietaire`) VALUES
-(1, 'Oeuvre en pret 1', 1000),
-(2, 'Oeuvre en pret 2', 1000);
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `oeuvrevente`
 --
 
@@ -78,44 +58,22 @@ CREATE TABLE `oeuvrevente` (
   `id_oeuvrevente` int(10) UNSIGNED NOT NULL,
   `titre_oeuvrevente` varchar(200) NOT NULL,
   `etat_oeuvrevente` varchar(1) NOT NULL,
-  `prix_oeuvrevente` float NOT NULL,
-  `id_proprietaire` int(10) UNSIGNED DEFAULT NULL
+  `prix_oeuvrevente` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `oeuvrevente`
 --
 
-INSERT INTO `oeuvrevente` (`id_oeuvrevente`, `titre_oeuvrevente`, `etat_oeuvrevente`, `prix_oeuvrevente`, `id_proprietaire`) VALUES
-(10000, 'lala', 'L', 12, 1000),
-(10001, 'Ete', 'R', 400, 1000),
-(10002, 'Automne', 'L', 10, 1000),
-(10003, 'Crepuscule', 'R', 111, 1001),
-(10004, 'Hiver', 'R', 234, 1000),
-(10005, 'Aurore', 'L', 654, 1002),
-(10006, 'Nuit de printemps', 'L', 789, 1001),
-(10007, 'nouvelle Oeuvre 2', 'L', 12, 1000);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `proprietaire`
---
-
-CREATE TABLE `proprietaire` (
-  `id_proprietaire` int(10) UNSIGNED NOT NULL,
-  `nom_proprietaire` varchar(100) CHARACTER SET latin1 NOT NULL,
-  `prenom_proprietaire` varchar(100) CHARACTER SET latin1 DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `proprietaire`
---
-
-INSERT INTO `proprietaire` (`id_proprietaire`, `nom_proprietaire`, `prenom_proprietaire`) VALUES
-(1000, 'DUPONT', 'Isabelle'),
-(1001, 'DUPUIS', 'Valerie'),
-(1002, 'MOZART', 'Albert');
+INSERT INTO `oeuvrevente` (`id_oeuvrevente`, `titre_oeuvrevente`, `etat_oeuvrevente`, `prix_oeuvrevente`) VALUES
+(10000, 'lala', 'L', 12),
+(10001, 'Ete', 'R', 400),
+(10002, 'Automne', 'L', 10),
+(10003, 'Crepuscule', 'R', 111),
+(10004, 'Hiver', 'R', 234),
+(10005, 'Aurore', 'L', 654),
+(10006, 'Nuit de printemps', 'L', 789),
+(10007, 'nouvelle Oeuvre 2', 'L', 12);
 
 -- --------------------------------------------------------
 
@@ -150,24 +108,10 @@ ALTER TABLE `adherent`
   ADD PRIMARY KEY (`id_adherent`);
 
 --
--- Index pour la table `oeuvrepret`
---
-ALTER TABLE `oeuvrepret`
-  ADD PRIMARY KEY (`id_oeuvrepret`),
-  ADD KEY `id_proprietaire` (`id_proprietaire`);
-
---
 -- Index pour la table `oeuvrevente`
 --
 ALTER TABLE `oeuvrevente`
-  ADD PRIMARY KEY (`id_oeuvrevente`),
-  ADD KEY `id_proprietaire` (`id_proprietaire`);
-
---
--- Index pour la table `proprietaire`
---
-ALTER TABLE `proprietaire`
-  ADD PRIMARY KEY (`id_proprietaire`);
+  ADD PRIMARY KEY (`id_oeuvrevente`);
 
 --
 -- Index pour la table `reservation`
@@ -187,20 +131,10 @@ ALTER TABLE `reservation`
 ALTER TABLE `adherent`
   MODIFY `id_adherent` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
--- AUTO_INCREMENT pour la table `oeuvrepret`
---
-ALTER TABLE `oeuvrepret`
-  MODIFY `id_oeuvrepret` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
 -- AUTO_INCREMENT pour la table `oeuvrevente`
 --
 ALTER TABLE `oeuvrevente`
   MODIFY `id_oeuvrevente` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10008;
---
--- AUTO_INCREMENT pour la table `proprietaire`
---
-ALTER TABLE `proprietaire`
-  MODIFY `id_proprietaire` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1003;
 --
 -- AUTO_INCREMENT pour la table `reservation`
 --
@@ -209,18 +143,6 @@ ALTER TABLE `reservation`
 --
 -- Contraintes pour les tables exportées
 --
-
---
--- Contraintes pour la table `oeuvrepret`
---
-ALTER TABLE `oeuvrepret`
-  ADD CONSTRAINT `oeuvrepret_ibfk_1` FOREIGN KEY (`id_proprietaire`) REFERENCES `proprietaire` (`id_proprietaire`);
-
---
--- Contraintes pour la table `oeuvrevente`
---
-ALTER TABLE `oeuvrevente`
-  ADD CONSTRAINT `oeuvrevente_ibfk_1` FOREIGN KEY (`id_proprietaire`) REFERENCES `proprietaire` (`id_proprietaire`);
 
 --
 -- Contraintes pour la table `reservation`
